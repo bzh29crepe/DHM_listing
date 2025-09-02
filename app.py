ff426cdfc6295456918ef10d0ea89ff81fbcdb18
louislb1302 <81574402+bzh29crepe@users.noreply.github.com>
2025-09-02 09:18:53 +0200

    Delete image

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import streamlit as st
 
 df = pd.read_csv("DHMLISTING Complet.csv", encoding="latin1", sep=";")
 
+pb = ("B", "P", "BH", "B &")
+
+df = df[df["Location"].isin(pb)].reset_index()
+
 st.set_page_config(page_title="DHM LISTING", layout="wide")
 
 st.title("DHM LISTING")
